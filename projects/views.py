fef368fe22249dc44b650df1269b2b08cb53ab97
Mark Parmelee <mparm920@gmail.com>
2019-10-21 18:47:39 -0400

    section 4

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
 def project_list(request):
     return render(request, 'projects/index.html')
+
+def all_projects(request):
+    projects = Project.objects.all()
+    print(projects)
+    return render(request, 'projects/all_projects.html', {'projects': projects})
