9e06937d9d43176d10f65e7e131c74580fb4b215
Mark Parmelee <mparm920@gmail.com>
2019-10-21 23:43:57 -0400

    added bold models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+def Category(models.Model):
+    name = models.CharField(max_length=20)
+
+def Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)    
+    category = models.ManyToManyField('Category', related_name='posts')
+
+def Comment(models.Model):
+    author = models.CharField(max_length=20)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)    
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
