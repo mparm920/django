f01f65b1ca83496233289d1956626660924ec3a5
Mark Parmelee <mparm920@pop-os.localdomain>
2019-11-15 08:28:43 -0500

    updated Models and made migrations

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-def Category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=20)
 
-def Post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)    
     category = models.ManyToManyField('Category', related_name='posts')
 
-def Comment(models.Model):
+class Comment(models.Model):
     author = models.CharField(max_length=20)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)    
